dd81e33f5a4c0c01ae706dd3595ec7b30dd6284e
David-LBautista <196w0007@itszongolica.edu.mx>
2020-01-06 14:08:44 -0600

    rutas hijas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,20 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
+import { UsuarioComponent } from './components/usuario/usuario.component';
+import { UsuarioNuevoComponent } from './components/usuario/usuario-nuevo.component';
+import { UsuarioEditarComponent } from './components/usuario/usuario-editar.component';
+import { UsuarioDetalleComponent } from './components/usuario/usuario-detalle.component';
+import { USUARIO_ROUTES } from './components/usuario/usuario.routes';
 
 
 const routes: Routes = [
   { path: 'home', component: HomeComponent },
+  { 
+    path: 'usuario/:id', 
+    component: UsuarioComponent,
+    children: USUARIO_ROUTES
+  },
   {path: '**', pathMatch:'full', redirectTo:'home'}
 
 ];
